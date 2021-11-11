e66c5b277caf53e04c89388cf630fb375ce92908
gangzhe <60089644+gangzhe@users.noreply.github.com>
2021-11-11 15:01:55 +0800

    dbs logo

--- frontend/webapp/src/App.js
+++ frontend/webapp/src/App.js
@@ -4,11 +4,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from "./Components/Login";
 import { Redirect } from "react-router";
+import Logo from "./dbs_logo.svg";
 
 function App() {
     return (
         <Router>
             <div className="App">
+                <div style={{ display: "flex", justifyContent: "flex-end" }}>
+                    <img src={Logo} alt="logo" />
+                </div>
                 <Switch>
                     {/* <Redirect to="/login" /> */}
                     <Route exact path="/login">
