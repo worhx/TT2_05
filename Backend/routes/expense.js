60736843db853b82e82cf60010ae86178b605d73
BburnN123 <huijie_1995@hotmail.com>
2021-11-11 15:00:26 +0800

    Added new expenses

--- Backend/routes/expense.js
+++ Backend/routes/expense.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { create } from "../controllers/expense.js";
+import { create, get, update, deleteExpenses } from "../controllers/expense.js";
 const expenseRoute = express.Router({ mergeParams: true });
 
-//all routes in here start with /login
+//CREATE EXPENSE
 expenseRoute.post('/', create);
+
+//GET EXPENSE
+expenseRoute.get('/', get);
+
+//UPDATE EXPENSE
+expenseRoute.put('/:expensesId', update);
+
+//DELETE EXPENSE
+expenseRoute.delete('/', deleteExpenses);
 
 export default expenseRoute;
