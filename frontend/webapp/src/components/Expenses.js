5bfd53acdad9a978f5012c54839ecd16b818f4c7
BburnN123 <huijie_1995@hotmail.com>
2021-11-11 16:45:55 +0800

    Added Expense table

--- frontend/webapp/src/components/Expenses.js
+++ frontend/webapp/src/components/Expenses.js
@@ -1,29 +1,63 @@
-import Expense from './Expense'
-import { useState } from 'react'
+import Expense from './Expense';
+import { useEffect, useState } from 'react';
+import ExpensesSvc from "../Services/expenses.js"
+import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
 
 const Expenses = () => {
-    const [expenses, setExpenses] = useState([{
-        id: 1,
-        projectId: 1,
-        catId: 1
-    },
-    {
-        id: 2,
-        projectId: 2,
-        catId: 2
-    }])
+    const [expenses, setExpenses] = useState([])
 
-    const addExpense = (expense) => {
-        setExpenses([...expenses, expense])
-    }
+    // const addExpense = (expense) => {
+    //     setExpenses([...expenses, expense])
+    // }
 
-    
+    useEffect(() => {
+        getExpenses()
+    }, []);
+
+    const getExpenses = async () => {
+
+        const listOfExpenses = await new ExpensesSvc().get("2");
+        setExpenses(listOfExpenses.data);
+
+    };
 
     return (
-        <div>
-            {expenses.map((expense) => (
-                <Expense key={expense.id} expense={expense}/>
-            ))}
+
+        <div style={{ marginTop: "50px" }}>
+
+            <Table striped bordered hover size="sm">
+
+                <thead>
+
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                        <th>Amount</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {expenses.map((_expense) => (
+                        <tr>
+
+                            <td>{_expense.id}</td>
+                            <td>{_expense.name}</td>
+                            <td>{_expense.description}</td>
+                            <td>{_expense.amount}</td>
+
+                            <td>
+                                <Button>Add</Button>
+
+                                <Button>Edit</Button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+
+
         </div>
     )
 }
