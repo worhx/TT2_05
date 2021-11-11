42aba2a44316c349555c7b006689218fea203eed
KER BENG HIAN <benghian.ker@ncs.com.sg>
2021-11-11 12:31:30 +0800

    Added get all category

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import cors from 'cors';//prevent cors error
 import loginRoute from './routes/login.js ';
-
+import categoryRoute from './routes/category.js';
 
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use('/login',loginRoute);
+app.use('/category',categoryRoute);
 app.get('/', (req,res)=>{
   res.send('Hello from home page');
 })
