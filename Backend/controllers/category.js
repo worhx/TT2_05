2a14a553a053fd54724d660fe91c6080f3e2b310
KER BENG HIAN <benghian.ker@ncs.com.sg>
2021-11-11 15:46:47 +0800

    added controller try catch

--- Backend/controllers/category.js
+++ Backend/controllers/category.js
@@ -3,7 +3,6 @@ import {Category} from "../models/category.js"
 export const getAllCategory = async (req,res)=>{
   try{
     const data = await Category.findAll();
-    res.send(data);
     if(data){
         console.log("Success");
         return res.status(200).json(data);
