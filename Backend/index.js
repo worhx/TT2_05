9d17b9ba2d9c3eb0c2773d3acb4e645cc01a2b55
BburnN123 <huijie_1995@hotmail.com>
2021-11-11 14:02:57 +0800

    Added expenses url

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';//prevent cors error
 import loginRoute from './routes/login.js ';
 import categoryRoute from './routes/category.js';
 import userRoute from './routes/user.js';
+import expenseRoute from './routes/expense.js';
 
 const app = express();
 const PORT = 5000;
@@ -13,6 +14,7 @@ app.use(cors());
 app.use('/login',loginRoute);
 app.use('/category',categoryRoute);
 app.use('/user',userRoute);
+app.use('/expenses',expenseRoute);
 app.get('/', (req,res)=>{
   res.send('Hello from home page');
 })
