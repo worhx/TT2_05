9c07f2bb509c537cd6aead15b1f878d5ce23b245
BburnN123 <huijie_1995@hotmail.com>
2021-11-11 15:49:00 +0800

    Created category, expenses and project services

---
+++ frontend/webapp/src/Services/expenses.js
@@ -0,0 +1,28 @@
+import axios from "axios";
+import React from "react";
+
+const baseURL = "worhx.ddns.net";
+
+class ExpensesSvc extends React.PureComponent {
+    async get(projectId) {
+        const expensesList = await axios.get(`${baseURL}/projects/${projectId}/expenses`)
+        return expensesList
+    }
+
+    async create(projectId, projectData) {
+        const expensesList = await axios.post(`${baseURL}/projects/${projectId}/expenses`, projectData)
+        return expensesList
+    }
+
+    async update(projectId, expensesId, projectData) {
+        const expensesList = await axios.put(`${baseURL}/projects/${projectId}/expenses/${expensesId}`, projectData)
+        return expensesList
+    }
+
+    async delete(projectId, expenseId) {
+        const response = await axios.get(`${baseURL}/projects/${projectId}/expenses?id=${expenseId}`)
+        return response
+    }
+}
+
+export default ExpensesSvc;
---
+++ frontend/webapp/src/Services/project.js
@@ -0,0 +1,29 @@
+
+import axios from "axios";
+
+const baseURL = "worhx.ddns.net";
+
+class ProjectSvc {
+
+    // GET ALL PROJECTS
+    async get() {
+
+        const loginUser = await axios.get(`${baseURL}/projects`)
+        return loginUser;
+    }
+
+    // GET BY ID
+    async getById(username, password) {
+        const loginUser = await axios.post(`${baseURL}/projects`,
+            {
+                "username": username,
+                "password": password
+            }
+        )
+
+        return loginUser;
+    }
+}
+
+export default ProjectSvc;
+
