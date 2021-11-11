345a0e346daf15ffe9abc31f19ccd0284b26c2b3
tzzzening <e0324455@u.nus.edu>
2021-11-11 13:12:24 +0800

    basic expenses

--- frontend/webapp/src/App.js
+++ frontend/webapp/src/App.js
@@ -1,12 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Expenses from './components/Expenses'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      
     </div>
   );
 }
---
+++ frontend/webapp/src/components/Expenses.js
@@ -0,0 +1,11 @@
+import Expense from './Expense'
+
+const Expenses = () => {
+    return (
+        <div>
+            <Expense />
+        </div>
+    )
+}
+
+export default Expenses
