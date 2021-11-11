9f5f7e878e4725b40c73983edf16650688ddb535
KER BENG HIAN <benghian.ker@ncs.com.sg>
2021-11-11 12:42:48 +0800

    Added return project tied to user

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';//prevent cors error
 import loginRoute from './routes/login.js ';
 import categoryRoute from './routes/category.js';
+import userRoute from './routes/user.js';
 
 const app = express();
 const PORT = 5001;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(cors());
 app.use('/login',loginRoute);
 app.use('/category',categoryRoute);
+app.use('/user',userRoute);
 app.get('/', (req,res)=>{
   res.send('Hello from home page');
 })
