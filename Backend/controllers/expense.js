a0272aaecb73c7801d8e8f2154ed511c1475830f
BburnN123 <huijie_1995@hotmail.com>
2021-11-11 14:40:05 +0800

    Added update, delete, retrieve controller

--- Backend/controllers/expense.js
+++ Backend/controllers/expense.js
@@ -2,19 +2,68 @@ import { Expense } from "../models/expense.js"
 
 export const create = async (req, res) => {
 
-  const expenseData = req.body
-
-  if (expenseData.amount) {
-    expenseData.amount = parseFloat(expenseData.amount);
-  }
-
   try {
+
+    const expenseData = req.body
+
+    if (expenseData.amount) {
+      expenseData.amount = parseFloat(expenseData.amount);
+    }
+
     const _expense = await Expense.create(
       expenseData
     )
     if (!_expense) {
       throw new Error("Expenses not created")
     }
+    res.json(_expense)
+  }
+  catch (err) {
+    console.log(err);
+    res.status(403).send(err);
+  }
+}
+
+export const get = async (req, res) => {
+
+  try {
+    const _expense = await Expense.findAll()
+    res.json(_expense)
+  }
+  catch (err) {
+    console.log(err);
+    res.status(403).send(err);
+  }
+}
+
+export const update = async (req, res) => {
+
+  try {
+    const expenseData = req.body
+
+    if (expenseData.amount) {
+      expenseData.amount = parseFloat(expenseData.amount);
+    }
+
+    const _expense = await Expense.update(
+      expenseData
+    )
+    res.json(_expense)
+  }
+  catch (err) {
+    console.log(err);
+    res.status(403).send(err);
+  }
+}
+
+export const deleteExpenses = async (req, res) => {
+
+  try {
+    const _expense = await deleteExpenses.destroy({
+      where: {
+        "id": req.query.id
+      }
+    })
     res.json(_expense)
   }
   catch (err) {
